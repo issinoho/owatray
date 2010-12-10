a0fe14d5004d8ac855210e31097fca95f318249c
Iain Smith <irs@cygnets.co.uk>
2010-12-10 12:14:36 +0000

    Added changelog to repo

---
+++ ChangeLog.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DrunkenBakery.OWAtray
+{
+    partial class ChangeLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangeLog));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 303);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(305, 277);
+            this.listBox1.TabIndex = 0;
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Location = new System.Drawing.Point(254, 321);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(75, 23);
+            this.cmdOK.TabIndex = 1;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
+            // 
+            // ChangeLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 354);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ChangeLog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Change Log";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.ListBox listBox1;
+
+    }
+}
---
+++ ChangeLog.cs
@@ -0,0 +1,64 @@
+﻿//------------------------------------------------------------------
+// DrunkenBakery OWA Tray Monitor
+// ChangeLog Form
+//
+// <copyright file="ContactUs.cs" company="The Drunken Bakery">
+//     Copyright (c) 2009, 2010 The Drunken Bakery. All rights reserved.
+// </copyright>
+//
+// Form to provide an RSS feed of changes scraped from the website.
+//
+//------------------------------------------------------------------
+namespace DrunkenBakery.OWAtray
+{
+    using System;
+    using System.ServiceModel.Syndication;
+    using System.Windows.Forms;
+    using System.Xml;
+
+    /// <summary>
+    /// Displays a Change Log scraped from an RSS Feed
+    /// </summary>
+    public partial class ChangeLog : Form
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangeLog"/> class.
+        /// </summary>
+        public ChangeLog(string rssUrl)
+        {
+            InitializeComponent();
+
+            try
+            {
+                listBox1.Items.Clear();
+                XmlReader reader = XmlReader.Create(rssUrl);
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    listBox1.Items.Add(item.Title.Text);
+                    listBox1.Items.Add(item.PublishDate.ToString("dd MMMM yyyy, hh:mm:ss") + " | " + item.Authors[0].Email + " (" + item.Authors[0].Name + ")");
+                    listBox1.Items.Add(string.Empty);
+                }
+
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void cmdOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion Methods
+    }
+}
